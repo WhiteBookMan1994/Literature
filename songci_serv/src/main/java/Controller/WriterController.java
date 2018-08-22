2d593638a9aede7ff0b99fab63e7e787ed690a74
dingchenchen <dccaijtt@163.com>
2018-08-22 23:26:08 +0800

    连接数据库

---
+++ songci_serv/src/main/java/Controller/WriterController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "writer")
+public class WriterController {
+
+    @PostMapping(value = "/add")
+    public boolean addWriter(@RequestParam String name, @RequestParam String birthday){
+
+        return true;
+    }
+}
